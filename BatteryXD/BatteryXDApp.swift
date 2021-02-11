4f711a9c0be4a828c8b7577f4843a96155343304
Shakshi3104 <pikanchisato@yahoo.co.jp>
2021-02-11 09:04:15 +0900

    Initial Commit

---
+++ BatteryXD/BatteryXDApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryXDApp.swift
+//  BatteryXD
+//
+//  Created by MacBook Pro on 2021/02/11.
+//
+
+import SwiftUI
+
+@main
+struct BatteryXDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BatteryXD/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BatteryXD
+//
+//  Created by MacBook Pro on 2021/02/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
