d5280e0b977f244ec56144cda6e5688ea54cdba9
Shakshi3104 <watchosastrom3104@gmail.com>
2021-03-05 10:10:25 +0900

    Add gradient in ring

--- BatteryXD/View/BatteryRingView.swift
+++ BatteryXD/View/BatteryRingView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct BatteryRing: View {
     var batteryLevel: Float
     
+    private let darkGreen = Color(red: 0.367, green: 0.807, blue: 0.389)
+    
     var body: some View {
         ZStack {
             // background
@@ -21,10 +23,30 @@ struct BatteryRing: View {
             // battery level
             Circle()
                 .trim(from: 0.0, to: CGFloat(batteryLevel))
-                .stroke(style: StrokeStyle(lineWidth: 15.0, lineCap: .round, lineJoin: .round))
+                .stroke(AngularGradient(
+                    gradient: Gradient(colors: [self.darkGreen, Color.green]),
+                    center: .center,
+                    startAngle: .degrees(0),
+                    endAngle: .degrees(360)
+                ),
+                    style: StrokeStyle(lineWidth: 15.0, lineCap: .round, lineJoin: .round))
                 .foregroundColor(.green)
                 .rotationEffect(Angle(degrees: 270.0))
                 .animation(.linear)
+            
+            // starting point
+            Circle()
+                .frame(width: 15.0, height: 15.0)
+                .foregroundColor(self.darkGreen)
+                .offset(y: -60)
+            
+            // ending point
+            Circle()
+                .frame(width: 15.0, height: 15.0)
+                .offset(y: -60)
+                .foregroundColor(batteryLevel > 0.95 ? Color.green : Color.green.opacity(0))
+                .rotationEffect(Angle.degrees(360 * Double(batteryLevel)))
+                
         }.frame(width: 120, height: 120, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
     }
 }
@@ -56,7 +78,7 @@ struct BatteryRingCharging: View {
 struct BatteryRingView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            BatteryRingCharging(batteryLevel: 0.8, batteryCharging: true)
+            BatteryRingCharging(batteryLevel: 0.8, batteryCharging: false)
         }
     }
 }
